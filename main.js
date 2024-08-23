c07933fa6942584b1a325481d656eb3ea92a576d
emma437 <410208044@gms.ndhu.edu.tw>
2024-08-24 00:41:32 +0800

    首頁

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 console.log("Hello world!");
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 24,
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  pagination: {
+    el: ".swiper-pagination-custom",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+  breakpoints: {
+    991: {
+      slidesPerView: 2,
+      spaceBetween: 48,
+    },
+  },
+});
